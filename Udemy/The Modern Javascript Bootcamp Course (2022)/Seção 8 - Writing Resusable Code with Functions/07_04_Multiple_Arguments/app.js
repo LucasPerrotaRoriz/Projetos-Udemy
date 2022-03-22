51a3422eb9a955a77cd1bc9203ccfd7f237fefe7
Lucas Perrota <98526798+LucasPerrotaRoriz@users.noreply.github.com>
2022-03-22 15:15:41 -0300

    Seção 6, 7, 8, 9

---
+++ Udemy/The Modern Javascript Bootcamp Course (2022)/Seção 8 - Writing Resusable Code with Functions/07_04_Multiple_Arguments/app.js
@@ -0,0 +1,14 @@
+function square(num) {
+	console.log(num * num);
+}
+
+function sum(x, y) {
+	console.log(x + y);
+}
+
+function divide(a, b) {
+	console.log(a / b);
+}
+
+divide(1,4);
+divide(4,1);
